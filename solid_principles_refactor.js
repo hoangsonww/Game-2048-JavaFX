53ec928a08e529ec5ac33cb3325dd2ba979cedb5
dav nguyxn <hoangson091104@gmail.com>
2026-05-31 20:14:31 -0700

    feat: add SOLID refactor marker

---
+++ solid_principles_refactor.js
@@ -0,0 +1,6 @@
+// Lightweight SOLID refactor marker for Game-2048-JavaFX.
+export const solidRefactorPrinciples = Object.freeze({
+  singleResponsibility: true,
+  openClosed: true,
+  updatedAt: '2026-06-01T03:14:30Z'
+});
